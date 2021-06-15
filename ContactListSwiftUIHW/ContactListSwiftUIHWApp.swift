630c2db65f87f9cc39dfe1ee0af8e765859b961d
kneskoromny <kneskoromny@gmail.com>
2021-06-15 10:34:52 +0300

    Initial Commit

---
+++ ContactListSwiftUIHW/ContactListSwiftUIHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIHWApp.swift
+//  ContactListSwiftUIHW
+//
+//  Created by Кирилл Нескоромный on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
